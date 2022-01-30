ae6ba3357ceb2fec73cbf70e86d9b697f09b3d79
Shrung <87622226+ShrungDN@users.noreply.github.com>
2022-01-30 12:27:48 +0530

    Added files

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+
+# import tensorflow as tf
+
+print(tf.__version__)
+# model = tf.keras.models.load_model('finalmodel')
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
